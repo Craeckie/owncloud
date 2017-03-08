7ab5b57f333075a207e98d073f8b7f794f305768
Josia <josia-internet@rosskopfs.de>
2017-03-08 15:19:05 +0100

    My local additions

--- 9.0-php7/build.sh
+++ 9.0-php7/build.sh
@@ -1,4 +1,10 @@
 cd "`dirname \"$0\"`"
+<<<<<<< 41ee1903ed0689ffe8550ffd85e696316d9f8513
 git pull --rebase upstream master
 #docker pull php:7.0-apache
 docker build --pull -t owncloud:9.0.8-apache-php7 .
+=======
+git pull
+#docker pull php:7.0-apache
+docker build -t owncloud:9.0.5-apache-php7 .
+>>>>>>> My local additions
