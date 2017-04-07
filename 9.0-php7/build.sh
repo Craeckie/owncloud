a79f0d113cbe60c34fe4946faf97661e68bbeb33
Josia <josia-internet@rosskopfs.de>
2017-04-07 13:55:23 +0200

    Updated ownCloud, included pull --rebase in build.sh

--- 9.0-php7/build.sh
+++ 9.0-php7/build.sh
@@ -1,4 +1,4 @@
 cd "`dirname \"$0\"`"
-git pull
+git pull --rebase upstream master
 #docker pull php:7.0-apache
-docker build -t owncloud:9.0.5-apache-php7 .
+docker build --pull -t owncloud:9.0.8-apache-php7 .
